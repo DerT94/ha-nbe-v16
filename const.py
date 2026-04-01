5b8b57778761d735627428566af49b22cd6c758d
root <root@a0d7b954-ssh.local.hass.io>
2026-04-01 11:40:50 +0200

    Initial commit: Neues Blueprint Grundgerüst

---
+++ const.py
@@ -0,0 +1,8 @@
+"""Constants for nbe_v16."""
+
+from logging import Logger, getLogger
+
+LOGGER: Logger = getLogger(__package__)
+
+DOMAIN = "nbe_v16"
+ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
